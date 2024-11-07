07ae5eba9bd6e7950c4f9a50a997a95e21906627
Jessen Forbush <jessenforbush@gmail.com>
2024-11-07 12:16:47 -0700

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+class Program{
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Welcome to this Calculator App!");
+
+}
+
+public void menu(){
+    
+
+}
+
+
+
+
+}
